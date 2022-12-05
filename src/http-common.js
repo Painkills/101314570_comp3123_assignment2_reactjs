9c30780ce0afe797742b28dd2d70853241041e93
Painkills <72031894+Painkills@users.noreply.github.com>
2022-12-05 10:35:32 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://101314570-comp-3123-backend.vercel.app/api",
   headers: {
-    "Access-Control-Allow-Origin": "*"
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-    "Access-Control-Allow-Methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"]
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     Authorization: 'Bearer DtLDDYNQAbRO48QmFJO9pssD',
     "Content-type": "application/json"
   }
