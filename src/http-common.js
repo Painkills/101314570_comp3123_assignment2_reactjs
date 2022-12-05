cb8e8d1cd38a4b301885cec81b5a78e1966af03d
Painkills <72031894+Painkills@users.noreply.github.com>
2022-12-05 10:15:56 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://101314570-comp-3123-backend.vercel.app/api",
   headers: {
+    Authorization: 'Bearer DtLDDYNQAbRO48QmFJO9pssD',
     "Content-type": "application/json"
   }
 });
